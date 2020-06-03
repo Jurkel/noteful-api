70999c969adc53b76d020a316888cc1504adb8ac
Harvey Le <harveyxcle@gmail.com>
2020-06-03 13:06:48 -0400

    added folders and notes folder

---
+++ src/folders/folders-service.js
@@ -0,0 +1,18 @@
+const FoldersService = {
+  list(knex) {
+    return knex('folders').select('*');
+  },
+
+  findById(knex, id) {
+    return knex('folders').where({id}).first('*');
+  },
+
+  insert(knex, folder) {
+    return knex('folders')
+      .insert(folder)
+      .returning('*')
+      .then(rows => rows[0]);
+  }
+};
+
+module.exports = FoldersService;
